893088f2e07b757d653d9807c643a71ac80caa23
Chanjeong Park <ckswjd99@gmail.com>
2023-10-16 20:02:17 +0900

    Reached 530 GFLOPS.

--- HW2/hw2/matmul/matmul.c
+++ HW2/hw2/matmul/matmul.c
@@ -11,13 +11,13 @@
 #define MAX_THREADS 256
 
 /* HYPERPARAMS */
-static const int MAC_M = 64;
-static const int MAC_N = 64;
-static const int MAC_K = 4096;
+#define MAC_M 64
+#define MAC_N 64
+#define MAC_K 4096
 
-static const int MIC_M = 16;
-static const int MIC_N = 16;
-static const int MIC_K = 8;
+#define MIC_M 16
+#define MIC_N 16
+#define MIC_K 8
 
 struct thread_arg {
   const float *A;
@@ -47,9 +47,6 @@ void matmul_singlethread(float *A, float *B, float *C, size_t M, size_t N, size_
 static void sgemm_reg(int M, int N, int K, float *A, float *B, float *C, int lda, int ldb, int ldc) {
   float cbuf[MAC_M * MAC_N];
 
-  int mac_M = MAC_M;
-  int mac_N = MAC_N;
-
   // Regular M
   for (int mac_m = 0; mac_m < M; mac_m += MAC_M) {
     // Regular N
@@ -58,13 +55,13 @@ static void sgemm_reg(int M, int N, int K, float *A, float *B, float *C, int lda
 
       #if __AVX512F__
       // MACRO KERNEL - AVX512
-      // Macro kernel with AVX512
-      for (int thr_k = 0; thr_k < K; thr_k++) {
+      // Implemented with AVX512
+      for (int k = 0; k < K; k++) {
         for (int m = 0; m < MAC_M; m++) {
-          __m512 avec = _mm512_set1_ps(A[(mac_m + m) * K + thr_k]);
+          __m512 avec = _mm512_set1_ps(A[(mac_m + m) * K + k]);
 
           for (int n = 0; n < MAC_N; n += MIC_N) {
-            __m512 bvec = _mm512_loadu_ps(&B[thr_k * ldb + (mac_n + n)]);
+            __m512 bvec = _mm512_loadu_ps(&B[k * ldb + (mac_n + n)]);
             __m512 cvec = _mm512_loadu_ps(&cbuf[m * MAC_N + n]);
             cvec = _mm512_fmadd_ps(avec, bvec, cvec);
             _mm512_storeu_ps(&cbuf[m * MAC_N + n], cvec);
@@ -74,11 +71,11 @@ static void sgemm_reg(int M, int N, int K, float *A, float *B, float *C, int lda
 
       #else
       // MACRO KERNEL - NAIVE
-      // Implemented code is naive, but compiler will automatically optimize here with AVX codes.
-      for (int thr_k = 0; thr_k < K; thr_k++) { 
+      // Implemented code is naive, but compiler will automatically optimize here.
+      for (int k = 0; k < K; k++) { 
         for (int m = 0; m < MAC_M; m++) {
           for (int n = 0; n < MAC_N; n++) {
-            cbuf[m * MAC_N + n] += A[(mac_m + m) * K + thr_k] * B[thr_k * ldb + (mac_n + n)];
+            cbuf[m * MAC_N + n] += A[(mac_m + m) * K + k] * B[k * ldb + (mac_n + n)];
           }
         }
       }
@@ -108,46 +105,62 @@ static void sgemm(int M, int N, int K, float *A, float *B, float *C, int lda, in
   }
 
   // Regular M
-  for (int thr_m = 0; thr_m < (M - MAC_M + 1); thr_m += MAC_M) {
+  for (int mac_m = 0; mac_m < (M - MAC_M + 1); mac_m += MAC_M) {
     // Regular N
-    for (int thr_n = 0; thr_n < (N - MAC_N + 1); thr_n += MAC_N) {
+    for (int mac_n = 0; mac_n < (N - MAC_N + 1); mac_n += MAC_N) {
       bzero(cbuf, sizeof(float) * MAC_M * MAC_N);
 
-      /* MACRO KERNEL - NAIVE */
-      for (int thr_k = 0; thr_k < K; thr_k++) { 
-        
+      #if __AVX512F__
+      // MACRO KERNEL - AVX512
+      // Implemented with AVX512
+      for (int k = 0; k < K; k++) {
         for (int m = 0; m < MAC_M; m++) {
-          for (int n = 0; n < MAC_N; n++) {
-            cbuf[m * MAC_N + n] += A[(thr_m + m) * K + thr_k] * B[thr_k * ldb + (thr_n + n)];
+          __m512 avec = _mm512_set1_ps(A[(mac_m + m) * K + k]);
+
+          for (int n = 0; n < MAC_N; n += MIC_N) {
+            __m512 bvec = _mm512_loadu_ps(&B[k * ldb + (mac_n + n)]);
+            __m512 cvec = _mm512_loadu_ps(&cbuf[m * MAC_N + n]);
+            cvec = _mm512_fmadd_ps(avec, bvec, cvec);
+            _mm512_storeu_ps(&cbuf[m * MAC_N + n], cvec);
           }
         }
       }
 
-      for (int m = 0; m < MAC_M; m++) {
-        for (int n = 0; n < MAC_N; n++) {
-          C[(thr_m + m) * ldc + (thr_n + n)] = cbuf[m * MAC_N + n];
+      #else
+      // MACRO KERNEL - NAIVE
+      // Implemented code is naive, but compiler will automatically optimize here.
+      for (int k = 0; k < K; k++) { 
+        for (int m = 0; m < MAC_M; m++) {
+          for (int n = 0; n < MAC_N; n++) {
+            cbuf[m * MAC_N + n] += A[(mac_m + m) * K + k] * B[k * ldb + (mac_n + n)];
+          }
         }
+      }
+      #endif
+
+      for (int m = 0; m < MAC_M; m++) {
+        memcpy(&C[(mac_m + m) * ldc + (mac_n)], &cbuf[m * MAC_N], sizeof(float) * MAC_N);
       }
     }
 
     // Remaining N
     if (N % MAC_N != 0) {
-      int thr_n = (N / MAC_N) * MAC_N;
+      int mac_n = (N / MAC_N) * MAC_N;
       bzero(cbuf, sizeof(float) * MAC_M * MAC_N);
 
       /* MACRO KERNEL - NAIVE */
-      for (int thr_k = 0; thr_k < K; thr_k++) {
+      for (int k = 0; k < K; k++) {
 
         for (int m = 0; m < MAC_M; m++) {
-          for (int n = 0; n < N - thr_n; n++) {
-            cbuf[m * MAC_N + n] += A[(thr_m + m) * K + thr_k] * B[thr_k * ldb + (thr_n + n)];
+          for (int n = 0; n < N - mac_n; n++) {
+            cbuf[m * MAC_N + n] += A[(mac_m + m) * K + k] * B[k * ldb + (mac_n + n)];
           }
         }
       }
 
       for (int m = 0; m < MAC_M; m++) {
-        for (int n = 0; n < N - thr_n; n++) {
-          C[(thr_m + m) * ldc + (thr_n + n)] = cbuf[m * MAC_N + n];
+        for (int n = 0; n < N - mac_n; n++) {
+          C[(mac_m + m) * ldc + (mac_n + n)] = cbuf[m * MAC_N + n];
         }
       }
     }
@@ -156,23 +169,23 @@ static void sgemm(int M, int N, int K, float *A, float *B, float *C, int lda, in
   
   // Remaining M
   if (M % MAC_M != 0) {
-    int thr_m = (M / MAC_M) * MAC_M;
+    int mac_m = (M / MAC_M) * MAC_M;
     // Regular N
-    for (int thr_n = 0; thr_n < (N - MAC_N + 1); thr_n += MAC_N) {
+    for (int mac_n = 0; mac_n < (N - MAC_N + 1); mac_n += MAC_N) {
       bzero(cbuf, sizeof(float) * MAC_M * MAC_N);
 
       /* MICRO KERNEL - NAIVE */
-      for (int thr_k = 0; thr_k < K; thr_k++) { 
+      for (int k = 0; k < K; k++) { 
         for (int m = 0; m < MAC_M; m++) {
           for (int n = 0; n < MAC_N; n++) {
-            cbuf[m * MAC_N + n] += A[(thr_m + m) * K + thr_k] * B[thr_k * ldb + (thr_n + n)];
+            cbuf[m * MAC_N + n] += A[(mac_m + m) * K + k] * B[k * ldb + (mac_n + n)];
           }
         }
       }
 
       for (int m = 0; m < MAC_M; m++) {
         for (int n = 0; n < MAC_N; n++) {
-          C[(thr_m + m) * ldc + (thr_n + n)] = cbuf[m * MAC_N + n];
+          C[(mac_m + m) * ldc + (mac_n + n)] = cbuf[m * MAC_N + n];
         }
       }
     }
@@ -180,21 +193,21 @@ static void sgemm(int M, int N, int K, float *A, float *B, float *C, int lda, in
     // Remaining N
 
     if (N % MAC_N != 0) {
-      int thr_n = (N / MAC_N) * MAC_N;
+      int mac_n = (N / MAC_N) * MAC_N;
       bzero(cbuf, sizeof(float) * MAC_M * MAC_N);
 
       /* MICRO KERNEL - NAIVE */
-      for (int thr_k = 0; thr_k < K; thr_k++) {
+      for (int k = 0; k < K; k++) {
         for (int m = 0; m < MAC_M; m++) {
-          for (int n = 0; n < N - thr_n; n++) {
-            cbuf[m * MAC_N + n] += A[(thr_m + m) * K + thr_k] * B[thr_k * ldb + (thr_n + n)];
+          for (int n = 0; n < N - mac_n; n++) {
+            cbuf[m * MAC_N + n] += A[(mac_m + m) * K + k] * B[k * ldb + (mac_n + n)];
           }
         }
       }
 
       for (int m = 0; m < MAC_M; m++) {
-        for (int n = 0; n < N - thr_n; n++) {
-          C[(thr_m + m) * ldc + (thr_n + n)] = cbuf[m * MAC_N + n];
+        for (int n = 0; n < N - mac_n; n++) {
+          C[(mac_m + m) * ldc + (mac_n + n)] = cbuf[m * MAC_N + n];
         }
       }
     }
