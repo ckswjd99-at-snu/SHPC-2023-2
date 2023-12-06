a8947eec143e035a9be8ca9c9445f69d658380d4
Chanjeong Park <ckswjd99@gmail.com>
2023-12-06 09:28:33 +0900

    OpenMP & concurrent streams - 16022 GFLOPS

--- HW6/hw6/matmul/matmul.cu
+++ HW6/hw6/matmul/matmul.cu
@@ -5,19 +5,21 @@
  *  Total latency under 0.11 sec
  *  
  *  [OPT LOG]
- *  Last optimization:  use only 1 node!
- *   - Using multiple nodes is not efficient because of MPI overhead
- *  Baseline latency:   0.157520 sec
+ *  Last optimization: interleave memcpy and kernel & OpenMP
+ *   - make stream_mem using only for memory copy
+ *   - handle multiple GPU streams using OpenMP
+ *  Baseline latency:   0.137291 sec
  * 
  *  [LATENCY BREAKDOWN]
- *  0. total latency:   0.157520 sec (100.0%)
- *  1. MPI_Scatter:     0.000000 sec (  0.0%)
- *  2. MPI_Bcast:       0.000000 sec (  0.0%)
- *  3. cudaMemcpyAsync: 0.052137 sec ( 33.1%)
- *  4. matmul_kernel:   0.058474 sec ( 37.1%)
- *  5. cudaMemcpyAsync: 0.020299 sec ( 12.9%)
- *  6. MPI_Gather:      0.000000 sec ( 0.0%)
- *  7. etc(error):      0.026610 sec ( 16.9%)
+ *  0. total latency:       0.137291 sec (100.0%)
+ *  1. MPI_Scatter:         0.000000 sec (  0.0%)
+ *  2. MPI_Bcast:           0.000000 sec (  0.0%)
+ *  3. cudaMemcpyAsync(B):  0.008148 sec (  5.9%)
+ *  3. cudaMemcpyAsync(A):  0.027521 sec ( 20.0%)
+ *  4. matmul_kernel:       0.091986 sec ( 67.0%)
+ *  5. cudaMemcpyAsync(C):  0.000004 sec (  0.0%)
+ *  6. MPI_Gather:          0.000000 sec (  0.0%)
+ *  7. etc(error):          0.009632 sec (  7.0%)
  * 
  *  => Plan: optimize kernel
  *  
@@ -26,6 +28,7 @@
 #include "matmul.h"
 #include "util.h"
 
+#include <omp.h>
 #include <cuda_runtime.h>
 #include <mpi.h>
 #include <pthread.h>
@@ -68,6 +71,7 @@ int M_node_start, M_node_end, M_node_size;
 static int Mbegin[NGPU], Mend[NGPU];
 static int ngpu;
 static cudaStream_t streams[NGPU];
+static cudaStream_t streams_mem[NGPU];
 static float *A_gpu[NGPU], *B_gpu[NGPU], *C_gpu[NGPU];
 
 
@@ -136,57 +140,82 @@ static __global__ void matmul_kernel(
 
 void matmul(const float *A, const float *B, float *C, int M, int N, int K) {
 
+  #ifdef MULTI_NODE
   // Scatter mat A
-  // float *Abuf = (float *)A;
-  // if (mpi_rank == 0) {
-  //   MPI_Scatter(
-  //     A, M * K / mpi_world_size, MPI_FLOAT, 
-  //     MPI_IN_PLACE, M * K / mpi_world_size,
-  //     MPI_FLOAT, 0, MPI_COMM_WORLD
-  //   );
-  // }
-  // else {
-  //   MPI_Scatter(
-  //     NULL, M * K / mpi_world_size, MPI_FLOAT, 
-  //     Abuf + K * M_node_start, M * K / mpi_world_size,
-  //     MPI_FLOAT, 0, MPI_COMM_WORLD
-  //   );
-  // }
+  float *Abuf = (float *)A;
+  if (mpi_rank == 0) {
+    MPI_Scatter(
+      A, M * K / mpi_world_size, MPI_FLOAT, 
+      MPI_IN_PLACE, M * K / mpi_world_size,
+      MPI_FLOAT, 0, MPI_COMM_WORLD
+    );
+  }
+  else {
+    MPI_Scatter(
+      NULL, M * K / mpi_world_size, MPI_FLOAT, 
+      Abuf + K * M_node_start, M * K / mpi_world_size,
+      MPI_FLOAT, 0, MPI_COMM_WORLD
+    );
+  }
 
   // Broadcast mat B
-  // float *Bbuf = (float *)B;
-  // MPI_Bcast(Bbuf, K * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  float *Bbuf = (float *)B;
+  MPI_Bcast(Bbuf, K * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  #endif
 
-  // Async memcpy H->D on each GPU
+  const int NUM_WORKLOAD = 1;
+
+  #pragma omp parallel for num_threads(ngpu)
   for (int i = 0; i < ngpu; i++) {
-    CHECK_CUDA(cudaSetDevice(i));
-    CHECK_CUDA(cudaMemcpyAsync(
-      A_gpu[i], &A[Mbegin[i] * K],
-      (Mend[i] - Mbegin[i]) * K * sizeof(float),
-      cudaMemcpyHostToDevice, streams[i]
-    ));
     CHECK_CUDA(cudaMemcpyAsync(
       B_gpu[i], B, K * N * sizeof(float),
-      cudaMemcpyHostToDevice, streams[i]
+      cudaMemcpyHostToDevice, streams_mem[i]
     ));
   }
 
-  // Run kernels asynchronously on each GPU
+  #pragma omp parallel for num_threads(ngpu)
   for (int i = 0; i < ngpu; i++) {
     CHECK_CUDA(cudaSetDevice(i));
-    dim3 blockDim(NUM_BDIM);
-    dim3 gridDim(CEIL_DIV(Mend[i] - Mbegin[i], BLOCK_M), CEIL_DIV(N, BLOCK_N));
-    matmul_kernel<<<gridDim, blockDim, 0, streams[i]>>>(
-        A_gpu[i], B_gpu[i], C_gpu[i], Mend[i] - Mbegin[i], N, K);
-    CHECK_CUDA(cudaGetLastError());
-  }
 
-  // Async memcpy D->H on each GPU
-  for (int i = 0; i < ngpu; i++) {
-    CHECK_CUDA(cudaSetDevice(i));
-    CHECK_CUDA(cudaMemcpyAsync(&C[Mbegin[i] * N], C_gpu[i],
-                               (Mend[i] - Mbegin[i]) * N * sizeof(float),
-                               cudaMemcpyDeviceToHost, streams[i]));
+    for (int wl=0; wl<NUM_WORKLOAD; wl++) {
+      cudaEvent_t htod_event, dtoh_event;
+
+      cudaEventCreate(&htod_event);
+      cudaEventCreate(&dtoh_event);
+
+      int Mbegin_wl = Mbegin[i] + wl * (Mend[i] - Mbegin[i]) / NUM_WORKLOAD;
+      int Mend_wl   = Mbegin[i] + (wl + 1) * (Mend[i] - Mbegin[i]) / NUM_WORKLOAD;
+      
+      // Async memcpy H->D on each GPU
+      CHECK_CUDA(cudaMemcpyAsync(
+        A_gpu[i], &A[Mbegin_wl * K],
+        (Mend_wl - Mbegin_wl) * K * sizeof(float),
+        cudaMemcpyHostToDevice, streams_mem[i]
+      ));
+
+      CHECK_CUDA(cudaEventRecord(htod_event, streams_mem[i]));
+      CHECK_CUDA(cudaStreamWaitEvent(streams[i], htod_event, 0));
+
+      // Run kernels asynchronously on each GPU
+      dim3 blockDim(NUM_BDIM);
+      dim3 gridDim(CEIL_DIV(Mend_wl - Mbegin_wl, BLOCK_M), CEIL_DIV(N, BLOCK_N));
+      matmul_kernel<<<gridDim, blockDim, 0, streams[i]>>>(
+          A_gpu[i], B_gpu[i], C_gpu[i], Mend_wl - Mbegin_wl, N, K);
+      CHECK_CUDA(cudaGetLastError());
+
+      CHECK_CUDA(cudaEventRecord(dtoh_event, streams[i]));
+      CHECK_CUDA(cudaStreamWaitEvent(streams_mem[i], dtoh_event, 0));
+
+      // Async memcpy D->H on each GPU
+      CHECK_CUDA(cudaMemcpyAsync(
+        &C[Mbegin_wl * N], C_gpu[i],
+        (Mend_wl - Mbegin_wl) * N * sizeof(float),
+        cudaMemcpyDeviceToHost, streams_mem[i]
+      ));
+
+      cudaEventDestroy(htod_event);
+      cudaEventDestroy(dtoh_event);
+    }
   }
 
   // Wait for all async jobs to finish
@@ -195,19 +224,22 @@ void matmul(const float *A, const float *B, float *C, int M, int N, int K) {
     cudaStreamSynchronize(streams[i]);
   }
 
+
+  #ifdef MULTI_NODE
   // Gather mat C
-  // if (mpi_rank == 0) {
-  //   MPI_Gather(
-  //     MPI_IN_PLACE, M * N / mpi_world_size, MPI_FLOAT, 
-  //     C, M * N / mpi_world_size, MPI_FLOAT, 0, MPI_COMM_WORLD
-  //   );
-  // }
-  // else {
-  //   MPI_Gather(
-  //     C + N * M_node_start, M * N / mpi_world_size, MPI_FLOAT, 
-  //     NULL, M * N / mpi_world_size, MPI_FLOAT, 0, MPI_COMM_WORLD
-  //   );
-  // }
+  if (mpi_rank == 0) {
+    MPI_Gather(
+      MPI_IN_PLACE, M * N / mpi_world_size, MPI_FLOAT, 
+      C, M * N / mpi_world_size, MPI_FLOAT, 0, MPI_COMM_WORLD
+    );
+  }
+  else {
+    MPI_Gather(
+      C + N * M_node_start, M * N / mpi_world_size, MPI_FLOAT, 
+      NULL, M * N / mpi_world_size, MPI_FLOAT, 0, MPI_COMM_WORLD
+    );
+  }
+  #endif
 }
 
 
@@ -237,6 +269,7 @@ void matmul_initialize(int M, int N, int K) {
   for (int i = 0; i < ngpu; i++) {
     CHECK_CUDA(cudaSetDevice(i));
     CHECK_CUDA(cudaStreamCreate(&streams[i]));
+    CHECK_CUDA(cudaStreamCreate(&streams_mem[i]));
   }
 
   for (int i = 0; i < ngpu; i++) {
@@ -257,5 +290,6 @@ void matmul_finalize() {
     CHECK_CUDA(cudaFree(B_gpu[i]));
     CHECK_CUDA(cudaFree(C_gpu[i]));
     CHECK_CUDA(cudaStreamDestroy(streams[i]));
+    CHECK_CUDA(cudaStreamDestroy(streams_mem[i]));
   }
 }
