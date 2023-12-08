2f9eb7cc2b1d3540abb7a2c1621f9cd1436d3ef4
Chanjeong Park <ckswjd99@gmail.com>
2023-12-08 22:20:56 +0900

    Compute engine - 1.96 input(s)/sec

--- FinalProject/classifier.cu
+++ FinalProject/classifier.cu
@@ -1,11 +1,15 @@
 #include <math.h>
+#include <omp.h>
 #include <mpi.h>
+#include <pthread.h>
 #include <cassert>
 
 #include "classifier.h"
 #include "util.h"
 
 static int mpi_rank;
+
+/** SECTION: Tensor **/
 
 // Multi-dimensional matrix containing fp32 elements
 struct Tensor {
@@ -50,100 +54,8 @@ void Tensor::fill_zeros() {
   for (int n = 0; n < N_; ++n) { buf[n] = 0.0; }
 }
 
-// Parameters
-Tensor *w_conv1, *w_conv2, *w_conv3, *w_conv4, *w_conv5, *w_conv6, *b_conv1,
-    *b_conv2, *b_conv3, *b_conv4, *b_conv5, *b_conv6, *w_fc1, *w_fc2, *w_fc3,
-    *b_fc1, *b_fc2, *b_fc3, *gamma_conv1, *beta_conv1, *gamma_conv6, *beta_conv6;
 
-// Activations
-Tensor *a_conv1, *a_layernorm1, *a_relu1, *a_pool1;
-Tensor *a_conv2, *a_relu2, *a_pool2;
-Tensor *a_conv3, *a_relu3;
-Tensor *a_conv4, *a_relu4;
-Tensor *a_conv5, *a_relu5;
-Tensor *a_conv6, *a_layernorm6, *a_relu6, *a_pool6;
-Tensor *a_collapse;
-Tensor *a_linear1, *a_relu7;
-Tensor *a_linear2, *a_relu8;
-Tensor *a_linear3;
-
-// Operations
-void conv1d(Tensor *input, Tensor *weight, Tensor *bias, Tensor *output,
-            int stride, int padding, int dilation, bool has_bias);
-void relu(Tensor *input, Tensor *output);
-void maxpool1d(Tensor *input, Tensor *output, int kernel_size, int stride);
-void collapse(Tensor *input, Tensor *output);
-void linear(Tensor *input, Tensor *weight, Tensor *bias, Tensor *output,
-            bool has_bias);
-void layernorm(Tensor *input, Tensor *gamma, Tensor *beta, Tensor *output);
-
-// Only the first process (root, mpi_rank == 0) has the input and output
-// Parallelization method is totally up to you, but you should gather 
-// the output at rank 0
-void classifier(float *input_, float *output_, int N) {
-  if (mpi_rank == 0) {
-    for (int n = 0; n < N; ++n) {  // N input sentences
-
-      // Load one input sentence from input
-      Tensor *one_input = new Tensor({1, VOCAB_SIZE, MAX_LENGTH}, input_ + n * VOCAB_SIZE * MAX_LENGTH);
-
-      // Conv block 1 : Conv1d + LayerNorm + ReLU + MaxPool1d
-      conv1d(one_input, w_conv1, b_conv1, a_conv1, 1, 0, 1, true);
-      layernorm(a_conv1, gamma_conv1, beta_conv1, a_layernorm1);
-      relu(a_layernorm1, a_relu1);
-      maxpool1d(a_relu1, a_pool1, 3, 3);
-
-      // Conv block 2 : Conv1d + ReLU + MaxPool1d
-      conv1d(a_pool1, w_conv2, b_conv2, a_conv2, 1, 0, 1, true);
-      relu(a_conv2, a_relu2);
-      maxpool1d(a_relu2, a_pool2, 3, 3);
-
-      // Conv block 3 : Conv1d + ReLU
-      conv1d(a_pool2, w_conv3, b_conv3, a_conv3, 1, 0, 1, true);
-      relu(a_conv3, a_relu3);
-
-      // Conv block 4 : Conv1d + ReLU
-      conv1d(a_relu3, w_conv4, b_conv4, a_conv4, 1, 0, 1, true);
-      relu(a_conv4, a_relu4);
-
-      // Conv block 5 : Conv1d + ReLU
-      conv1d(a_relu4, w_conv5, b_conv5, a_conv5, 1, 0, 1, true);
-      relu(a_conv5, a_relu5);
-
-      // Conv block 6 : Conv1d + LayerNorm + ReLU + MaxPool1d
-      conv1d(a_relu5, w_conv6, b_conv6, a_conv6, 1, 0, 1, true);
-      layernorm(a_conv6, gamma_conv6, beta_conv6, a_layernorm6);
-      relu(a_layernorm6, a_relu6);
-      maxpool1d(a_relu6, a_pool6, 3, 3);
-
-      // Collapse
-      collapse(a_pool6, a_collapse);
-
-      // FC block 1 : Linear + ReLU
-      linear(a_collapse, w_fc1, b_fc1, a_linear1, true);
-      relu(a_linear1, a_relu7);
-
-      // FC block 2 : Linear + ReLU
-      linear(a_relu7, w_fc2, b_fc2, a_linear2, true);
-      relu(a_linear2, a_relu8);
-
-      // FC block 3 : Linear
-      linear(a_relu8, w_fc3, b_fc3, a_linear3, true);
-
-      float max_val = -1e99f;
-      int max_idx = 0;
-      for (int i = 0; i < a_linear3->num_elem(); ++i) {
-        if (a_linear3->buf[i] > max_val) {
-          max_val = a_linear3->buf[i];
-          max_idx = i;
-        }
-      }
-
-      output_[n] = max_idx;
-    }  // end N input sentences loop
-  }    // if mpi_rank == 0
-}
-
+/** SECTION: Operators **/
 void conv1d(Tensor *input, Tensor *weight, Tensor *bias, Tensor *output,
             int stride = 1, int padding = 0, int dilation = 1,
             bool has_bias = true) {
@@ -236,108 +148,319 @@ void layernorm(Tensor *input, Tensor *gamma, Tensor *beta, Tensor *output) {
   }
 }
 
+
+/** SECTION: COMPUTE_ENGINE **/
+#define COMPUTE_BATCH_SIZE 10
+#define COMPUTE_MAX_QUEUE_SIZE 16
+
+struct ComputeEngine {
+public:
+  ComputeEngine(float *parameter, int num_input);
+  ~ComputeEngine();
+
+  void set_input(float *input_buf_);
+  void set_output(float *output_buf_);
+
+  void run();
+  void join();
+
+  void push(int num_input);
+
+private:
+  // Input
+  float *input_buf;
+  float *output_buf;
+  int num_input;
+  
+  // Queue
+  float *input_to_process;
+  int num_input_ready;
+  int num_input_processed;
+  pthread_mutex_t mutex_queue;
+  pthread_cond_t cond_queue;
+
+  int pop();
+  void inference(int num_input);
+
+  // Runner
+  static void *run_func(void *arg);
+  pthread_t thread;
+
+  // Parameters
+  Tensor *w_conv1, *w_conv2, *w_conv3, *w_conv4, *w_conv5, *w_conv6, *b_conv1,
+      *b_conv2, *b_conv3, *b_conv4, *b_conv5, *b_conv6, *w_fc1, *w_fc2, *w_fc3,
+      *b_fc1, *b_fc2, *b_fc3, *gamma_conv1, *beta_conv1, *gamma_conv6, *beta_conv6;
+
+  // Activations
+  Tensor *a_conv1, *a_layernorm1, *a_relu1, *a_pool1;
+  Tensor *a_conv2, *a_relu2, *a_pool2;
+  Tensor *a_conv3, *a_relu3;
+  Tensor *a_conv4, *a_relu4;
+  Tensor *a_conv5, *a_relu5;
+  Tensor *a_conv6, *a_layernorm6, *a_relu6, *a_pool6;
+  Tensor *a_collapse;
+  Tensor *a_linear1, *a_relu7;
+  Tensor *a_linear2, *a_relu8;
+  Tensor *a_linear3;
+};
+
+ComputeEngine *compute_engine;  // Singleton
+
+ComputeEngine::ComputeEngine(float *parameter_, int num_input_) {
+  if (compute_engine != nullptr) {
+    printf("ComputeEngine is a singleton class\n");
+    exit(1);
+  }
+  compute_engine = this;
+
+  // Initialize member variables
+  input_buf = nullptr;
+  output_buf = nullptr;
+  num_input = num_input_;
+
+  // Initialize queue
+  num_input_ready = 0;
+  num_input_processed = 0;
+  pthread_mutex_init(&mutex_queue, NULL);
+  pthread_cond_init(&cond_queue, NULL);
+
+  // Initialize parameters
+  w_conv1 = new Tensor({256, 70, 7}, parameter_ + OFFSET0);
+  b_conv1 = new Tensor({256}, parameter_ + OFFSET1);
+  gamma_conv1 = new Tensor({256, 1008}, parameter_ + OFFSET2);
+  beta_conv1 = new Tensor({256, 1008}, parameter_ + OFFSET3);
+  w_conv2 = new Tensor({256, 256, 7}, parameter_ + OFFSET4);
+  b_conv2 = new Tensor({256}, parameter_ + OFFSET5);
+  w_conv3 = new Tensor({256, 256, 3}, parameter_ + OFFSET6);
+  b_conv3 = new Tensor({256}, parameter_ + OFFSET7);
+  w_conv4 = new Tensor({256, 256, 3}, parameter_ + OFFSET8);
+  b_conv4 = new Tensor({256}, parameter_ + OFFSET9);
+  w_conv5 = new Tensor({256, 256, 3}, parameter_ + OFFSET10);
+  b_conv5 = new Tensor({256}, parameter_ + OFFSET11);
+  w_conv6 = new Tensor({256, 256, 3}, parameter_ + OFFSET12);
+  b_conv6 = new Tensor({256}, parameter_ + OFFSET13);
+  gamma_conv6 = new Tensor({256, 102}, parameter_ + OFFSET14);
+  beta_conv6 = new Tensor({256, 102}, parameter_ + OFFSET15);
+  w_fc1 = new Tensor({1024, 8704}, parameter_ + OFFSET16);
+  b_fc1 = new Tensor({1024}, parameter_ + OFFSET17);
+  w_fc2 = new Tensor({1024, 1024}, parameter_ + OFFSET18);
+  b_fc2 = new Tensor({1024}, parameter_ + OFFSET19);
+  w_fc3 = new Tensor({4, 1024}, parameter_ + OFFSET20);
+  b_fc3 = new Tensor({4}, parameter_ + OFFSET21);
+
+  // Initialize activations
+  a_conv1 = new Tensor({1, 256, 1008});
+  a_layernorm1 = new Tensor({1, 256, 1008});
+  a_relu1 = new Tensor({1, 256, 1008});
+  a_pool1 = new Tensor({1, 256, 336});
+  a_conv2 = new Tensor({1, 256, 330});
+  a_relu2 = new Tensor({1, 256, 330});
+  a_pool2 = new Tensor({1, 256, 110});
+  a_conv3 = new Tensor({1, 256, 108});
+  a_relu3 = new Tensor({1, 256, 108});
+  a_conv4 = new Tensor({1, 256, 106});
+  a_relu4 = new Tensor({1, 256, 106});
+  a_conv5 = new Tensor({1, 256, 104});
+  a_relu5 = new Tensor({1, 256, 104});
+  a_conv6 = new Tensor({1, 256, 102});
+  a_layernorm6 = new Tensor({1, 256, 102});
+  a_relu6 = new Tensor({1, 256, 102});
+  a_pool6 = new Tensor({1, 256, 34});
+  a_collapse = new Tensor({1, 8704});
+  a_linear1 = new Tensor({1, 1024});
+  a_relu7 = new Tensor({1, 1024});
+  a_linear2 = new Tensor({1, 1024});
+  a_relu8 = new Tensor({1, 1024});
+  a_linear3 = new Tensor({1, 4});
+}
+
+ComputeEngine::~ComputeEngine() {
+  pthread_mutex_destroy(&mutex_queue);
+  pthread_cond_destroy(&cond_queue);
+
+  delete w_conv1;
+  delete b_conv1;
+  delete w_conv2;
+  delete b_conv2;
+  delete w_conv3;
+  delete b_conv3;
+  delete w_conv4;
+  delete b_conv4;
+  delete w_conv5;
+  delete b_conv5;
+  delete w_conv6;
+  delete b_conv6;
+  delete w_fc1;
+  delete b_fc1;
+  delete w_fc2;
+  delete b_fc2;
+  delete w_fc3;
+  delete b_fc3;
+  delete gamma_conv1;
+  delete gamma_conv6;
+  delete beta_conv1;
+  delete beta_conv6;
+  delete a_conv1;
+  delete a_layernorm1;
+  delete a_relu1;
+  delete a_pool1;
+  delete a_conv2;
+  delete a_relu2;
+  delete a_pool2;
+  delete a_conv3;
+  delete a_relu3;
+  delete a_conv4;
+  delete a_relu4;
+  delete a_conv5;
+  delete a_relu5;
+  delete a_conv6;
+  delete a_layernorm6;
+  delete a_relu6;
+  delete a_pool6;
+  delete a_collapse;
+  delete a_linear1;
+  delete a_relu7;
+  delete a_linear2;
+  delete a_relu8;
+  delete a_linear3;
+}
+
+void ComputeEngine::set_input(float *input_buf_) {
+  input_buf = input_buf_;
+  input_to_process = input_buf;
+}
+
+void ComputeEngine::set_output(float *output_buf_) {
+  output_buf = output_buf_;
+}
+
+void ComputeEngine::run() {
+  pthread_create(&thread, NULL, ComputeEngine::run_func, this);
+}
+
+void ComputeEngine::join() {
+  pthread_join(thread, NULL);
+}
+
+void ComputeEngine::push(int num_input) {
+  pthread_mutex_lock(&mutex_queue);
+  if (num_input_ready == 0) pthread_cond_signal(&cond_queue);
+  num_input_ready += num_input;
+  pthread_mutex_unlock(&mutex_queue);
+}
+
+int ComputeEngine::pop() {
+  int num_input = 0;
+  pthread_mutex_lock(&mutex_queue);
+  if (num_input_ready == 0) pthread_cond_wait(&cond_queue, &mutex_queue);
+  num_input = std::min(num_input_ready, COMPUTE_BATCH_SIZE);
+  num_input_ready -= num_input;
+  pthread_mutex_unlock(&mutex_queue);
+  return num_input;
+}
+
+void ComputeEngine::inference(int num_input) {
+  
+  for (int batch = 0; batch < num_input; batch++) {
+    Tensor *input = new Tensor({1, VOCAB_SIZE, MAX_LENGTH}, input_to_process + batch * VOCAB_SIZE * MAX_LENGTH);
+
+    // Conv block 1 : Conv1d + LayerNorm + ReLU + MaxPool1d
+    conv1d(input, w_conv1, b_conv1, a_conv1, 1, 0, 1, true);
+    layernorm(a_conv1, gamma_conv1, beta_conv1, a_layernorm1);
+    relu(a_layernorm1, a_relu1);
+    maxpool1d(a_relu1, a_pool1, 3, 3);
+
+    // Conv block 2 : Conv1d + ReLU + MaxPool1d
+    conv1d(a_pool1, w_conv2, b_conv2, a_conv2, 1, 0, 1, true);
+    relu(a_conv2, a_relu2);
+    maxpool1d(a_relu2, a_pool2, 3, 3);
+
+    // Conv block 3 : Conv1d + ReLU
+    conv1d(a_pool2, w_conv3, b_conv3, a_conv3, 1, 0, 1, true);
+    relu(a_conv3, a_relu3);
+
+    // Conv block 4 : Conv1d + ReLU
+    conv1d(a_relu3, w_conv4, b_conv4, a_conv4, 1, 0, 1, true);
+    relu(a_conv4, a_relu4);
+
+    // Conv block 5 : Conv1d + ReLU
+    conv1d(a_relu4, w_conv5, b_conv5, a_conv5, 1, 0, 1, true);
+    relu(a_conv5, a_relu5);
+
+    // Conv block 6 : Conv1d + LayerNorm + ReLU + MaxPool1d
+    conv1d(a_relu5, w_conv6, b_conv6, a_conv6, 1, 0, 1, true);
+    layernorm(a_conv6, gamma_conv6, beta_conv6, a_layernorm6);
+    relu(a_layernorm6, a_relu6);
+    maxpool1d(a_relu6, a_pool6, 3, 3);
+
+    // Collapse
+    collapse(a_pool6, a_collapse);
+
+    // FC block 1 : Linear + ReLU
+    linear(a_collapse, w_fc1, b_fc1, a_linear1, true);
+    relu(a_linear1, a_relu7);
+
+    // FC block 2 : Linear + ReLU
+    linear(a_relu7, w_fc2, b_fc2, a_linear2, true);
+    relu(a_linear2, a_relu8);
+
+    // FC block 3 : Linear
+    linear(a_relu8, w_fc3, b_fc3, a_linear3, true);
+
+    float max_val = -1e99f;
+    int max_idx = 0;
+    for (int i = 0; i < a_linear3->num_elem(); ++i) {
+      if (a_linear3->buf[i] > max_val) {
+        max_val = a_linear3->buf[i];
+        max_idx = i;
+      }
+    }
+
+    output_buf[num_input_processed++] = max_idx;
+  }
+
+  input_to_process += num_input * VOCAB_SIZE * MAX_LENGTH;
+}
+
+void *ComputeEngine::run_func(void *arg) {
+  ComputeEngine *engine = (ComputeEngine *) arg;
+  while (engine->num_input_processed < engine->num_input) {
+    int num_input = engine->pop();
+    engine->inference(num_input);
+  }
+  return NULL;
+}
+
+
+/** SECTION: Classifier interface **/
+
 // load the parameter binary file and store parameters into Tensors
 // Only the first process (root, mpi_rank == 0) has the parameter
 // You must broadcast it to the others
 void initialize_classifier(float *parameter, int N) {
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  
   if (mpi_rank == 0) {
-    w_conv1 = new Tensor({256, 70, 7}, parameter + OFFSET0);
-    b_conv1 = new Tensor({256}, parameter + OFFSET1);
-    gamma_conv1 = new Tensor({256, 1008}, parameter + OFFSET2);
-    beta_conv1 = new Tensor({256, 1008}, parameter + OFFSET3);
-    w_conv2 = new Tensor({256, 256, 7}, parameter + OFFSET4);
-    b_conv2 = new Tensor({256}, parameter + OFFSET5);
-    w_conv3 = new Tensor({256, 256, 3}, parameter + OFFSET6);
-    b_conv3 = new Tensor({256}, parameter + OFFSET7);
-    w_conv4 = new Tensor({256, 256, 3}, parameter + OFFSET8);
-    b_conv4 = new Tensor({256}, parameter + OFFSET9);
-    w_conv5 = new Tensor({256, 256, 3}, parameter + OFFSET10);
-    b_conv5 = new Tensor({256}, parameter + OFFSET11);
-    w_conv6 = new Tensor({256, 256, 3}, parameter + OFFSET12);
-    b_conv6 = new Tensor({256}, parameter + OFFSET13);
-    gamma_conv6 = new Tensor({256, 102}, parameter + OFFSET14);
-    beta_conv6 = new Tensor({256, 102}, parameter + OFFSET15);
-    w_fc1 = new Tensor({1024, 8704}, parameter + OFFSET16);
-    b_fc1 = new Tensor({1024}, parameter + OFFSET17);
-    w_fc2 = new Tensor({1024, 1024}, parameter + OFFSET18);
-    b_fc2 = new Tensor({1024}, parameter + OFFSET19);
-    w_fc3 = new Tensor({4, 1024}, parameter + OFFSET20);
-    b_fc3 = new Tensor({4}, parameter + OFFSET21);
+    compute_engine = new ComputeEngine(parameter, N);
+  }
+}
 
-    a_conv1 = new Tensor({1, 256, 1008});
-    a_layernorm1 = new Tensor({1, 256, 1008});
-    a_relu1 = new Tensor({1, 256, 1008});
-    a_pool1 = new Tensor({1, 256, 336});
-    a_conv2 = new Tensor({1, 256, 330});
-    a_relu2 = new Tensor({1, 256, 330});
-    a_pool2 = new Tensor({1, 256, 110});
-    a_conv3 = new Tensor({1, 256, 108});
-    a_relu3 = new Tensor({1, 256, 108});
-    a_conv4 = new Tensor({1, 256, 106});
-    a_relu4 = new Tensor({1, 256, 106});
-    a_conv5 = new Tensor({1, 256, 104});
-    a_relu5 = new Tensor({1, 256, 104});
-    a_conv6 = new Tensor({1, 256, 102});
-    a_layernorm6 = new Tensor({1, 256, 102});
-    a_relu6 = new Tensor({1, 256, 102});
-    a_pool6 = new Tensor({1, 256, 34});
-    a_collapse = new Tensor({1, 8704});
-    a_linear1 = new Tensor({1, 1024});
-    a_relu7 = new Tensor({1, 1024});
-    a_linear2 = new Tensor({1, 1024});
-    a_relu8 = new Tensor({1, 1024});
-    a_linear3 = new Tensor({1, 4});
+// Only the first process (root, mpi_rank == 0) has the input and output
+// Parallelization method is totally up to you, but you should gather 
+// the output at rank 0
+void classifier(float *input_, float *output_, int N) {
+  if (mpi_rank == 0) {
+    compute_engine->set_input(input_);
+    compute_engine->set_output(output_);
+    compute_engine->run();
+    compute_engine->push(N);
+    compute_engine->join();
   }
 }
 
 // Free all dynamically allocated variables
 void finalize_classifier() {
   if (mpi_rank == 0) {
-    delete w_conv1;
-    delete b_conv1;
-    delete w_conv2;
-    delete b_conv2;
-    delete w_conv3;
-    delete b_conv3;
-    delete w_conv4;
-    delete b_conv4;
-    delete w_conv5;
-    delete b_conv5;
-    delete w_conv6;
-    delete b_conv6;
-    delete w_fc1;
-    delete b_fc1;
-    delete w_fc2;
-    delete b_fc2;
-    delete w_fc3;
-    delete b_fc3;
-    delete gamma_conv1;
-    delete gamma_conv6;
-    delete beta_conv1;
-    delete beta_conv6;
-    delete a_conv1;
-    delete a_layernorm1;
-    delete a_relu1;
-    delete a_pool1;
-    delete a_conv2;
-    delete a_relu2;
-    delete a_pool2;
-    delete a_conv3;
-    delete a_relu3;
-    delete a_conv4;
-    delete a_relu4;
-    delete a_conv5;
-    delete a_relu5;
-    delete a_conv6;
-    delete a_layernorm6;
-    delete a_relu6;
-    delete a_pool6;
-    delete a_collapse;
-    delete a_linear1;
-    delete a_relu7;
-    delete a_linear2;
-    delete a_relu8;
-    delete a_linear3;
+    delete compute_engine;
   }
 }
